28d288e1325c4d3512f79c74e17dc98cd9b2d627
InQuestDiscord <167710202+InQuestDiscord@users.noreply.github.com>
2024-04-21 14:12:03 -0500

    Update Settings.tsx

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -8,7 +8,7 @@ interface SettingsProps {
 
 export default ({ settings }: SettingsProps) => {
    return <FormRow
-      label='Example Setting'
+      label='InQuest Settings'
       trailing={
          <FormSwitch
             value={settings.get('example', true)}
